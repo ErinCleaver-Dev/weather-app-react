1fb2e3ef97e1e0aea62045ed3bcbc045b24616e7
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-05-03 21:24:13 -0400

    Added key

--- src/components/forecast/ForecastCard.js
+++ src/components/forecast/ForecastCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ForecastCard = ({day, min, max, condition ,icon}) => {
+const ForecastCard = ({id, day, min, max, condition ,icon}) => {
   return (
-    <div class="col-lg-2 col-md-3 col-sm-6 d-flex flex-column align-items-center weather_boarder daily">
+    <div id="id" class="col-lg-2 col-md-3 col-sm-6 d-flex flex-column align-items-center weather_boarder daily">
         <p class="day">{day}</p>
             <p class="day_temp"><span class="min">{min}</span>°/<span class="max">{max}</span>°<span class="convert">C</span></p>
             <p class="day_condition">{condition}</p>
